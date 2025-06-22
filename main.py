df3a5fed8a93c26cb58b1bc7a2d98613b08b618f
AhammedKabeer434 <ahammedkabeer17219330@gmail.com>
2025-06-22 21:58:55 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-print("Hello, world!")
-
-# To track the file in github => git add "file name"
-
-# To check the status of the file in github => git status
-
-# To make an commit file => git commit -m "message"
